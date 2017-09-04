38b5259b09837bbeb16f504c0e8fc01e64c6e2e3
beipiaoren <601664875@qq.com>
2017-09-04 11:48:46 +0800

    仪器设置

---
+++ app/src/main/java/com/dxc/mycollector/DeviceSettingActivity.java
@@ -0,0 +1,13 @@
+package com.dxc.mycollector;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DeviceSettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.device_setting);
+    }
+}
