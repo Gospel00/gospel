aebad4278552139f4925396adb62d1ec450d21f2
beipiaoren <601664875@qq.com>
2017-09-04 09:17:13 +0800

    首页activity

--- app/src/main/java/com/dxc/mycollector/PersonAcitvity.java
+++ app/src/main/java/com/dxc/mycollector/PersonAcitvity.java
@@ -1,5 +1,6 @@
 package com.dxc.mycollector;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -17,6 +18,7 @@ public class PersonAcitvity extends BaseActivity {
     TextView textView;
     String result = null;
    // private Button button;
+   private TextView titleName;
     private Handler handler = new Handler() {
         public void handleMessage(android.os.Message msg) {
             switch (msg.what) {
@@ -35,9 +37,20 @@ public class PersonAcitvity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.person_homepage_layout);
-        textView = (TextView) findViewById(R.id.textView4);
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setCustomView(R.layout.index_actionbar);
+
+//必须加2句
+        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);  //根据字面意思是显示类型为显示自定义
+        actionBar.setDisplayShowCustomEnabled(true); //自定义界面是否可显示
+
+//可动态改变标题内容，即获取到标题栏的控件
+
+        titleName = (TextView) findViewById(R.id.title_name);
+
+        //textView = (TextView) findViewById(R.id.textView4);
        // button=(Button)findViewById(R.id.button);
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+       /* findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                // startActivity(new Intent(PersonAcitvity.this, CeLiangActivity.class));
@@ -52,7 +65,7 @@ public class PersonAcitvity extends BaseActivity {
                     }
                 }.start();
             }
-        });
+        });*/
     }
 
 }
