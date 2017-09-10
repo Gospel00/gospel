62ce9860ba878fd454799aa039d1872d5cf03fe0
sunyi <yinglong.sun@hpe.com>
2017-09-10 20:14:35 +0800

    增加设备信息帮助类 调整首页点击 调整侧滑菜单样式
    Merge branch 'master' of https://github.com/Gospel00/gospel
    
    # Conflicts:
    #	app/src/main/java/com/dxc/mycollector/CeliangManualOperation.java

--- app/src/main/java/com/dxc/mycollector/MainActivity.java
+++ app/src/main/java/com/dxc/mycollector/MainActivity.java
@@ -2,6 +2,7 @@ package com.dxc.mycollector;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.ActivityManager;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
@@ -9,14 +10,20 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
+import android.os.StatFs;
 import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.text.format.Formatter;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -25,9 +32,17 @@ import android.widget.Toast;
 import com.dxc.mycollector.dbhelp.SqliteUtils;
 import com.dxc.mycollector.logs.Logger;
 import com.dxc.mycollector.taskDownload.DownLoadService;
+import com.dxc.mycollector.utils.DateConver;
+import com.dxc.mycollector.utils.DeviceUtil;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -42,6 +57,11 @@ public class MainActivity extends Activity implements
     private EditText username;
     private EditText lgpwd;
     Context context;
+    private LocationManager locationManager;// 位置管理类
+
+    private String provider;// 位置提供器
+
+
     private Dialog mWeiboDialog;//对话框
     private Handler mHandler = new Handler() {
         @Override
@@ -128,6 +148,173 @@ public class MainActivity extends Activity implements
                 startActivity(new Intent(MainActivity.this, RegisterAcitvity.class));
             }
         });
+
+        String weiyibiaoshi = "唯一标识：" + DeviceUtil.getDeviceId(context);
+        String pingpai = "设备品牌：" + DeviceUtil.getPhoneBrand();
+        String xinghao = "设备型号：" + DeviceUtil.getPhoneModel();
+        String api = "设备SDK：" + DeviceUtil.getBuildLevel() + "";
+        String sdkhao = "设备系统版本：" + DeviceUtil.getBuildVersion();
+        String sbhight = "设备尺寸高：" + DeviceUtil.deviceHeight(context) + "";
+        String sbwidht = "设备尺寸宽：" + DeviceUtil.deviceWidth(context) + "";
+        String kyncdx = "可运行内存：" + getAvailMemory();
+//        String zxtncdx = "总系统内存：" + getTotalMemory();
+        String zn = "总系统内存：" + getZm();
+        String deviceInfo = weiyibiaoshi + "\n" + pingpai + "\n" + xinghao +
+                "\n" + api + "\n" + sdkhao + "\n" + sbhight +
+                "\n" + sbwidht + "\n" + kyncdx + "\n" + zn;
+        Logger.i("DeviceInfo", deviceInfo);
+        // 更新当前位置
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        // 获得LocationManager的实例
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        // 获取所有可用的位置提供器
+        List<String> providerList = locationManager.getProviders(true);
+        if (providerList.contains(LocationManager.GPS_PROVIDER)) {
+            //优先使用gps
+            provider = LocationManager.GPS_PROVIDER;
+        } else if (providerList.contains(LocationManager.NETWORK_PROVIDER)) {
+            provider = LocationManager.NETWORK_PROVIDER;
+        } else {
+            // 没有可用的位置提供器
+            Toast.makeText(MainActivity.this, "没有位置提供器可供使用", Toast.LENGTH_LONG)
+                    .show();
+            Logger.i("DeviceInfo", "没有位置提供器可供使用");
+            return;
+        }
+
+        Location location = locationManager.getLastKnownLocation(provider);
+        if (location != null) {
+            // 显示当前设备的位置信息
+            Logger.i("DeviceInfo", "第一次请求位置信息");
+            showLocation(location, "");
+        } else {
+            Logger.i("DeviceInfo", "无法获得当前位置");
+        }
+        locationManager.requestLocationUpdates(provider, 30 * 1000, 1,
+                locationListener);
+
+
+    }
+
+    LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            // TODO Auto-generated method stub
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            // TODO Auto-generated method stub
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            // TODO Auto-generated method stub
+        }
+
+        @Override
+        public void onLocationChanged(Location location) {
+            // 设备位置发生改变时，执行这里的代码
+            String changeInfo = "隔10秒刷新的提示：\n 时间：" + DateConver.getStringDate()
+                    + ",\n当前的经度是：" + location.getLongitude() + ",\n 当前的纬度是："
+                    + location.getLatitude();
+            showLocation(location, changeInfo);
+        }
+    };
+
+    public String getZm() {
+        try {
+            final String mem_path = "/proc/meminfo";// 系统内存信息文件，第一行为内存大小
+            Reader reader = null;
+            BufferedReader bufferedReader = null;
+            reader = new FileReader(mem_path);
+            bufferedReader = new BufferedReader(reader, 8192);
+            long totalRAMSize = Long.parseLong(bufferedReader.readLine().split("\\s+")[1]) * 1024L;//这里*1024是转换为单位B（字节）
+            return "" + Formatter.formatFileSize(getBaseContext(), totalRAMSize);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    /**
+     * 显示当前设备的位置信息
+     *
+     * @param location
+     */
+    private String showLocation(Location location, String changeInfo) {
+        // TODO Auto-generated method stub
+        String currentLocation = "当前的经度是：" + location.getLongitude() + ",\n"
+                + "当前的纬度是：" + location.getLatitude();
+        Logger.i("DeviceInfo", currentLocation);
+        return currentLocation;
+    }
+
+    // 获取android当前可用内存大小
+    private String getAvailMemory() {
+        ActivityManager am = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        ActivityManager.MemoryInfo mi = new ActivityManager.MemoryInfo();
+        am.getMemoryInfo(mi);
+        //mi.availMem; 当前系统的可用内存
+
+        return Formatter.formatFileSize(getBaseContext(), mi.availMem);// 将获取的内存大小规格化
+    }
+
+    // 获得系统总内存
+    private String getTotalMemory() {
+        String str1 = "/proc/meminfo";// 系统内存信息文件
+        String str2;
+        String[] arrayOfString;
+        long initial_memory = 0;
+        try {
+            FileReader localFileReader = new FileReader(str1);
+            BufferedReader localBufferedReader = new BufferedReader(
+                    localFileReader, 8192);
+            str2 = localBufferedReader.readLine();// 读取meminfo第一行，系统总内存大小
+
+            arrayOfString = str2.split("\\s+");
+            for (String num : arrayOfString) {
+                Log.i(str2, num + "\t");
+            }
+            initial_memory = Integer.valueOf(arrayOfString[1]).intValue() * 1024;// 获得系统总内存，单位是KB，乘以1024转换为Byte
+            localBufferedReader.close();
+        } catch (IOException e) {
+        }
+        return Formatter.formatFileSize(getBaseContext(), initial_memory);// Byte转换为KB或者MB，内存大小规格化
+    }
+
+    /**
+     * 根据路径获取内存状态
+     *
+     * @param path
+     * @return
+     */
+    private String getMemoryInfo(File path) {
+        // 根据文件对象的路径获得一个磁盘状态对象
+        StatFs stat = new StatFs(path.getPath());
+
+        long blockSize = stat.getBlockSize();   // 获得一个扇区的大小
+
+        long totalBlocks = stat.getBlockCount();    // 获得扇区的总数
+
+        long availableBlocks = stat.getAvailableBlocks();   // 获得可用的扇区数量
+
+        // 总空间
+        String totalMemory = Formatter.formatFileSize(this, totalBlocks * blockSize);
+        // 可用空间
+        String availableMemory = Formatter.formatFileSize(this, availableBlocks * blockSize);
+
+        return "总空间: " + totalMemory + "\n可用空间: " + availableMemory;
     }
 
     public void initFolder() {
@@ -163,6 +350,7 @@ public class MainActivity extends Activity implements
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.READ_PHONE_STATE,
+            Manifest.permission.ACCESS_NETWORK_STATE,
             Manifest.permission.MOUNT_UNMOUNT_FILESYSTEMS
     };
 
--- app/src/main/java/com/dxc/mycollector/PersonAcitvity.java
+++ app/src/main/java/com/dxc/mycollector/PersonAcitvity.java
@@ -1,16 +1,28 @@
 package com.dxc.mycollector;
 
+import android.Manifest;
 import android.app.ActionBar;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.dxc.mycollector.logs.Logger;
+import com.dxc.mycollector.utils.DateConver;
+import com.dxc.mycollector.utils.DeviceUtil;
 
 /**
  * Created by gospel on 2017/8/18.
@@ -33,6 +45,20 @@ public class PersonAcitvity extends BaseActivity {
 
         context = this;
 
+        if (!DeviceUtil.isNetworkConnected(context)) {
+            Logger.i(TAG, "设备未联网...");
+            new AlertDialog.Builder(context)
+                    .setTitle("系统提示")
+                    .setCancelable(false)//按返回键不关闭窗口
+                    .setIcon(R.drawable.danger_small)
+                    .setMessage("系统检测到您的设备没有连接网络，任务将无法下载。")
+                    .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Logger.i(TAG, "已经提醒用户连接网络");
+                        }
+                    }).show();
+        }
         findViewById(R.id.clgl_layout).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -90,8 +116,8 @@ public class PersonAcitvity extends BaseActivity {
         if (Build.VERSION.SDK_INT >= 21) {
             getSupportActionBar().setElevation(0);
         }
-    }
 
+    }
     //按返回键到最后一个activity推出侧滑菜单
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
